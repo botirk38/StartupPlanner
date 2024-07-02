dbb2fc73b34ef259941ae4a6f921578bad89071f
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-07-02 22:50:15 +0100

    Start migrating to backend

--- startup-planner-frontend/app/api/canva/auth/route.ts
+++ startup-planner-frontend/app/api/canva/auth/route.ts
@@ -3,8 +3,6 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
 
-  const canvaAuthUrl = `https://www.canva.com/api/oauth/authorize?response_type=code&client_id=${process.env.CANVA_CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.CANVA_REDIRECT_URI)}&scope=asset:read asset:write design:content:read design:content:write design:meta:read profile:read &code_challenge=${codeChallenge}&code_challenge_method=S256&state=${state}`;
 
-  return NextResponse.redirect(canvaAuthUrl);
 }
 
