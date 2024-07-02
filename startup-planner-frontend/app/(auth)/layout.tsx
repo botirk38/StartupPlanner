97e93153a8b0386c0a0d3e4957a27c5bdaa59e63
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-07-02 15:56:26 +0100

    Optimize SEO for all pages

--- startup-planner-frontend/app/(auth)/layout.tsx
+++ startup-planner-frontend/app/(auth)/layout.tsx
@@ -3,12 +3,19 @@ import { cn } from '@/lib/utils'
 import '../globals.css'
 import { Toaster } from "@/components/ui/toaster";
 import Logo from '@/components/ui/logo';
+import { Metadata } from 'next';
 
 const fontHeading = Bricolage_Grotesque({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-heading',
 })
+
+export const metadata: Metadata = {
+  title: "Auth - Startup Planner",
+  description: "Access your Startup Planner account to streamline your business planning and management, or create a new account to start using our AI-driven tools.",
+  keywords: ["Login", "Register", "Startup Planner", "business planning", "entrepreneurs", "small business", "account access"],
+};
 
 
 export default function AuthLayout({
