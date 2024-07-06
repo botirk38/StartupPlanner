041574204abac42c35f81602ae5db869f6458b54
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-07-06 22:47:18 +0100

    Modify callback url for canva auth

--- startup_planner_backend/startup_planner_backend/urls.py
+++ startup_planner_backend/startup_planner_backend/urls.py
@@ -3,7 +3,7 @@ from canva_auth.views import canva_auth, canva_callback, contact_us, check_auth
 
 urlpatterns = [
     path('api/canva/auth/', canva_auth, name='canva-auth'),
-    path('api/canva/callback/', canva_callback, name='canva-callback'),
+    path('api/canva/auth/callback/', canva_callback, name='canva-callback'),
     path('api/contact-us/', contact_us, name='contact-us'),
     path('api/check-auth/', check_auth, name='check-auth'),
 ]
