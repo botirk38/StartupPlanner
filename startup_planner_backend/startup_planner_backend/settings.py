129cb6a7abeb645e5000180f7277a7f37a7d984b
Botir Khaltaev <ZLAC172@live.rhul.ac.uk>
2024-07-10 11:19:13 +0100

    Fix syntax errors

--- startup_planner_backend/startup_planner_backend/settings.py
+++ startup_planner_backend/startup_planner_backend/settings.py
@@ -102,10 +102,10 @@ DATABASES = {
 
 # CSRF
 
-CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "").split(",")
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "")
 
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "")
 
 
 # Password validation
